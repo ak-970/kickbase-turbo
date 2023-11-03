0026e5463776550f1fef84b7302ecdb941e05570
ak-970 <antje.kasemann@gmail.com>
2023-11-03 20:06:06 +0100

    carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,12 +2,14 @@ const Carousel = ({ children }) => {
 
   return (
     <div className='carousel'>
-      <div className='content'>
-        {children.map((child, index) =>
-          <div key={index} className='item'>
-            {child}
-          </div>
-        )}
+      <div className='content-wrap'>
+        <div className='content'>
+          {children.map((child, index) =>
+            <div key={index} className='item'>
+              {child}
+            </div>
+          )}
+        </div>
       </div>
       <div className='left'></div>
       <div className='right'></div>
